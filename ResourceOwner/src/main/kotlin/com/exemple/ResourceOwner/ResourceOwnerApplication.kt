0065e18ce348bff45a0d47da08f2526cc9c90bf6
Washington Bezerra <w.lsbezerra@ext.ppay.me>
2024-10-16 11:41:25 -0300

    feat: creating ResourceOwner structure

---
+++ ResourceOwner/src/main/kotlin/com/exemple/ResourceOwner/ResourceOwnerApplication.kt
@@ -0,0 +1,11 @@
+package com.exemple.ResourceOwner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResourceOwnerApplication
+
+    fun main(args: Array<String>) {
+        runApplication<ResourceOwnerApplication>(*args)
+    }
