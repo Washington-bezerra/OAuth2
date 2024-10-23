a77178be28b9e1a56ab10d73862c99ec791a4234
Washington Bezerra <w.lsbezerra@ext.ppay.me>
2024-10-23 08:40:46 -0300

    refact: renamed folder

--- AuthorizationServer/src/main/kotlin/com/example/authorizationServer/AuthorizationServerApplication.kt
+++ AuthorizationServer/src/main/kotlin/com/example/authorizationServer/AuthorizationServerApplication.kt
@@ -1,4 +1,4 @@
-package com.example.AuthorizationServer
+package com.example.authorizationServer
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
